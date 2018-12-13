5e3bef198fcf78c216681bccf2b69d9c51030196
matheusma37 <mtsma37@gmail.com>
2018-12-13 02:15:54 -0200

    Criando botão e campo para inserção de novos nomes, função de adicionar nome à lista de nomes e alguns ajustes de aparência na tela da lista.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,9 @@ import { NavController } from 'ionic-angular';
 })
 
 export class HomePage {
-  nomes: String = ["Matheus","João","Paulo"]
-  constructor(public navCtrl: NavController) {
+  nomes: String = ["Matheus","João","Paulo"];
+  nome: String = "";
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
 
@@ -16,5 +17,27 @@ export class HomePage {
   	if(nome){
   		this.nomes.splice( this.nomes.indexOf(nome), 1);
   	}
+  }
+
+  salvarNome(){
+  		if(!this.nome){
+  			let alert = this.alertCtrl.create({
+    			title: 'Falha ao inserir nome!',
+    			subTitle: 'Campo nome está vazio...',
+    			buttons: ['Ok']
+  			});
+  			alert.present();
+  		} else if (this.nomes.indexOf(this.nome) >= 0){
+  			let alert = this.alertCtrl.create({
+    			title: 'Falha ao inserir nome!',
+    			subTitle: 'Nome já existente na lista...',
+    			buttons: ['Ok']
+  			});
+  			this.nome = "";
+  			alert.present();
+  		}else{
+  			this.nomes.push(this.nome);
+  			this.nome = "";
+  		}
   }
 }
