6187c7fbe6d9bcca1b08897213f07a7f32d3916f
matheusma37 <mtsma37@gmail.com>
2018-12-16 10:40:04 -0200

    Modificando a tela contact e o contact.ts para fornecer as informações.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,10 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  nome: string = "Matheus Machado de Almeida"
+  matricula: string = "201611200091"
+  foto: string = "assets/imgs/foto.jpg"
+  
   constructor(public navCtrl: NavController) {
 
   }
